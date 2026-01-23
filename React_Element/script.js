d5e2b38e8aaabf6c389ea8700406f3ed6d9b1c72
kashif5677 <mdkashif5677@gmail.com>
2026-01-24 02:03:51 +0530

    update history

--- React_Element/script.js
+++ React_Element/script.js
@@ -1,9 +1,9 @@
-import h1 from './app.js'
+import box from './app.js'
 import circle from './test.js'
 
 let root = ReactDOM.createRoot(document.querySelector("#root"))
 
-const parent = React.createElement('div', null, [h1(), circle()])
+const parent = React.createElement('div', null, [box(), circle()])
 
 
 root.render(parent)
